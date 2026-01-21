55e082953822d4d6d9b48178fa3b1f6b7118f65c
Jbishopp9367floridapoly <Jbishopp9367@floridapoly.edu>
2026-01-21 01:54:28 +0000

    problem 1 solution

---
+++ p1.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using namespace std; 
+int main() {
+    cout << "  CCCC    OOOO    PPP " << endl; 
+    cout << " C       O    O   P  P" << endl;
+    cout << "C       O      O  PPP " << endl;
+    cout << " C       O    O   P   " << endl;
+    cout << "  CCCC    OOOO    P   " << endl;
+    return 0; 
+}
